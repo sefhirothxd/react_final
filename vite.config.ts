1df2d82cf2f49d537b3c4ceb3036969d0576b39f
bryan vera <bveracachay@gmail.com>
2022-08-22 01:05:08 -0500

    fix url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://sefhirothxd.github.io/',
+  base: 'https://sefhirothxd.github.io/react_final/',
   plugins: [react()],
 });
