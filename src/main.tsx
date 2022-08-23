4ccd24aaa6850a2502bb4069c3fefec2c3552bdb
bryan vera <bveracachay@gmail.com>
2022-08-22 20:11:04 -0500

    se borra deploy en github page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import UseProfileProvider from './context/UseProfileProvider';
 import './index.css';
 import 'flowbite';
@@ -9,7 +9,7 @@ import 'flowbite';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <UseProfileProvider>
-      <BrowserRouter basename="/react_final">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </UseProfileProvider>
